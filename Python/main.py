98166527c3bb98b585beff0ea4adf99e42ef09bd
Beatriz de Araújo Andrade <80129016+BeatrizA2@users.noreply.github.com>
2023-08-29 20:46:00 -0300

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -20,19 +20,28 @@ detector = PoseDetector()
 positions_folder = 'Positions files'
 os.makedirs(positions_folder, exist_ok=True)
 
+# Create the "Comparison files" folder if it doesn't exist
+comparison_folder = 'Comparison files'
+os.makedirs(comparison_folder, exist_ok=True)
+
 for video_file in video_files:
     video_path = os.path.join(videos_folder, video_file)
     video = cv.VideoCapture(video_path)
 
     positionsList = []
+    comparisonList = []
 
     while True:
         frameExist, img = video.read()
 
         if not frameExist:
             positions_filename = os.path.splitext(video_file)[0] + ".txt"
+
             positions_filepath = os.path.join(positions_folder, positions_filename)
+            comparison_filepath = os.path.join(comparison_folder, positions_filename)
+
             createFile(positions_filepath, positionsList)
+            createFile(comparison_filepath, comparisonList)
             break
 
         img = detector.findPose(img)
@@ -41,18 +50,27 @@ for video_file in video_files:
         # Verify if a person was detected
         if boundingBox:
             landmarkStr = ''
+            lmStr = ''
             for lm in landmarksList:
                 landmarkStr += f'{lm[1]},{img.shape[0] - lm[2]},{lm[3]},'
+                lmStr += f'{lm[1]},{lm[2]},{lm[3]},'
+
 
             positionsList.append(landmarkStr)
+            comparisonList.append(lmStr)
+
 
         cv.imshow("Image", img)
         key = cv.waitKey(1)
 
         if key == ord('s'):
             positions_filename = os.path.splitext(video_file)[0] + ".txt"
+
             positions_filepath = os.path.join(positions_folder, positions_filename)
+            comparison_filepath = os.path.join(comparison_folder, positions_filename)
+
             createFile(positions_filepath, positionsList)
+            createFile(comparison_filepath, comparisonList)
 
     video.release()
 
