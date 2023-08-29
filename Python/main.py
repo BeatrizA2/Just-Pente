f2e3f8073d140bf32f95e95d9cd6cbd877587f06
Beatriz Andrade <biaaraujo9925@hotmail.com>
2023-08-29 09:07:09 -0300

    create folder to receive the csv files

--- Python/main.py
+++ Python/main.py
@@ -1,38 +1,59 @@
+import os
 import cv2 as cv
 from cvzone.PoseModule import PoseDetector
 
-def createFile(positionsList):
-    with open("Positions.txt", 'w') as f:
+
+def createFile(filepath, positionsList):
+    with open(filepath, 'w') as f:
         f.writelines(["%s\n" % item for item in positionsList])
 
 
-video = cv.VideoCapture('Videos/cupid.mp4')
+# Path to the directory containing video files
+videos_folder = 'Videos/'
+
+# List all files in the folder
+video_files = [f for f in os.listdir(videos_folder) if f.endswith('.mp4')]
 
 detector = PoseDetector()
-positionsList = []
 
+# Create the "Positions files" folder if it doesn't exist
+positions_folder = 'Positions files'
+os.makedirs(positions_folder, exist_ok=True)
 
-while True:
-    frameExist, img = video.read()
+for video_file in video_files:
+    video_path = os.path.join(videos_folder, video_file)
+    video = cv.VideoCapture(video_path)
 
-    if not frameExist:
-        createFile(positionsList)
-        break
+    positionsList = []
 
-    img = detector.findPose(img)
-    landmarksList, boundingBox = detector.findPosition(img)
+    while True:
+        frameExist, img = video.read()
 
-    #Verify if a person was detected
-    if boundingBox:
-        landmarkStr = ''
-        for lm in landmarksList:
-            landmarkStr += f'{lm[1]},{img.shape[0] - lm[2]},{lm[3]},'
+        if not frameExist:
+            positions_filename = os.path.splitext(video_file)[0] + ".txt"
+            positions_filepath = os.path.join(positions_folder, positions_filename)
+            createFile(positions_filepath, positionsList)
+            break
 
-        positionsList.append(landmarkStr)
+        img = detector.findPose(img)
+        landmarksList, boundingBox = detector.findPosition(img)
 
+        # Verify if a person was detected
+        if boundingBox:
+            landmarkStr = ''
+            for lm in landmarksList:
+                landmarkStr += f'{lm[1]},{img.shape[0] - lm[2]},{lm[3]},'
 
-    cv.imshow("Image", img)
-    key = cv.waitKey(1)
+            positionsList.append(landmarkStr)
 
-    if key == ord('s'):
-        createFile(positionsList)
+        cv.imshow("Image", img)
+        key = cv.waitKey(1)
+
+        if key == ord('s'):
+            positions_filename = os.path.splitext(video_file)[0] + ".txt"
+            positions_filepath = os.path.join(positions_folder, positions_filename)
+            createFile(positions_filepath, positionsList)
+
+    video.release()
+
+cv.destroyAllWindows()
